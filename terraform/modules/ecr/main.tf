9f8778838eeeb8e1af08c95f9ba9efe9808ae181
chihiro723 <chihiroqianbofujibg@gmail.com>
2025-10-20 02:15:34 +0900

    fix(terraform): ECRリポジトリ名を小文字に統一
    - プロジェクト名を小文字に変換
    - AWS ECRの命名規則に準拠

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,7 +2,7 @@
 resource "aws_ecr_repository" "repositories" {
   for_each = toset(var.repositories)
 
-  name                 = "${var.project_name}-${var.environment}-${each.key}"
+  name                 = "${lower(var.project_name)}-${var.environment}-${each.key}"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
